89b6fc1bd6437b36128bc43e3318ab4009de3a78
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2022-12-19 11:13:58 +0330

    almost everything is done

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const root = document.querySelector(":root")
 const mainbody = document.querySelector("body")
-const beep = new Audio('./soundeffects/beep.mp3')
 const launchBtn = document.querySelector('.launch-btn')
 const spaceCraft = document.querySelector(".space-craft")
 const fire = document.querySelector(".fire-container")
 const sun = document.querySelector(".sun")
 const distanceDisplay = document.querySelector(".distance")
 const distanceTitle = document.querySelector(".distance-title")
+const popup = document.querySelector(".popup")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+const statusText = document.querySelector(".status")
+const planets = Array.from(document.querySelectorAll("#planet"))
 
-let lauchTimer = 11
+const beep = new Audio('./soundeffects/beep.mp3')
+const engine = new Audio('./soundeffects/rocket.mp3')
+engine.volume = 0.5
+engine.loop = 1
+
+let lauchTimer = 6
 let isPaused = true
-let finalDistance = 600000
+let finalDistance = 6000000000
 let startDistance = 0
 
 let timer
@@ -18,33 +27,39 @@ let distance
 
 distanceDisplay.classList.add("hide")
 distanceTitle.classList.add("hide")
+popup.classList.add("hide")
 
 launchBtn.addEventListener("click", openFullscreen)
+yesBtn.addEventListener("click", restartHandler)
+noBtn.addEventListener("click", popupHandler)
 
 function openFullscreen() {
+
+  planets.forEach((element) => {
+    element.style.display  = "inline"
+  })
 
   launchBtn.removeEventListener("click", openFullscreen)
   sun.style.top = "120vh";
   spaceCraft.style.top = "calc(55vh - 150px)"
-  launchBtn.style.top = "calc(90vh - 110px)";
+  launchBtn.style.top = "calc(90vh - 70px)";
   launchBtn.innerHTML = "launch";
   
   launchBtn.addEventListener("click", lauchHandler)
 }
 
-
 function lauchHandler () {
   distanceTitle.classList.remove("hide")
   launchBtn.removeEventListener("click", lauchHandler)
   isPaused = false
-  timer = setInterval(ignitionHandler,1000) 
+  timer = setInterval(ignitionHandler,1300) 
   launchBtn.addEventListener("click", abortHandler)
 }
 
 function abortHandler () {
+  popup.classList.remove("hide")
   clearInterval(timer)
   isPaused = true
-  launchBtn.addEventListener("click", lauchHandler)
 }
 
 function ignitionHandler () {
@@ -53,6 +68,7 @@ function ignitionHandler () {
     lauchTimer --
     launchBtn.innerHTML = `${lauchTimer} <span class="abort">abort</span>`
   } else {
+    engine.play()
     root.style.setProperty("--rocket-speed", "1000s")
     isPaused = true
     fire.style.opacity = "1"
@@ -64,22 +80,16 @@ function ignitionHandler () {
   }
 }
 
-function distanceHandler () {
-  distanceDisplay.classList.remove("hide")
-  distanceTitle.classList.add("hide")
-  if(startDistance < finalDistance) {
-    distanceDisplay.innerHTML = `${startDistance} km from sun`
-    startDistance += 100
-  } else {
-    distanceDisplay.innerHTML = "deep space"
-  }
-}
-
 function restartHandler () {
+  statusText.textContent = ""
+  statusText.style.opacity = "0"
+  engine.pause()
+  popup.classList.add("hide")
+  launchBtn.removeEventListener("click", abortHandler)
   launchBtn.removeEventListener("click", restartHandler)
   distanceDisplay.classList.add("hide")
   root.style.setProperty("--rocket-speed", "3000s")
-  lauchTimer = 11
+  lauchTimer = 6
   clearInterval(distance)
   clearInterval(timer)
   startDistance = 0
@@ -87,8 +97,60 @@ function restartHandler () {
   spaceCraft.style.top = "calc(0vh - 500px)"
   launchBtn.style.top = "calc(50vh - 70px)";
   launchBtn.innerHTML = "start mission";
+  planets.forEach((element) => {
+    element.style.display = "none"
+    element.style.top = "-50vw"
+  })
   launchBtn.addEventListener("click", openFullscreen)
   setTimeout(() => {
     fire.style.opacity = "0"
   },2500)
+}
+
+function popupHandler () {
+  popup.classList.add("hide")
+  lauchHandler()
+}
+
+function planetPass (index) {
+  planets[index].style.display  = "inline"
+  planets[index].style.top = "100%"
+  setTimeout(() => {
+    statusText.style.opacity = "1"
+    statusText.textContent = `we are passing the ${planets[index].getAttribute("alt")}`
+    setTimeout(() => {
+      statusText.style.opacity = "0"
+    },2500)
+  },3000)
+}
+
+function distanceHandler () {
+  distanceDisplay.classList.remove("hide")
+  distanceTitle.classList.add("hide")
+  if(startDistance < finalDistance) {
+    distanceDisplay.innerHTML = `${startDistance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} km from the sun`
+    startDistance += 20000
+  } else {
+    distanceDisplay.innerHTML = "deep space"
+  }
+ 
+  if (startDistance == 58000000) {
+    planetPass(0)
+  } else if (startDistance == 108000000) {
+    planetPass(1)
+  } else if (startDistance == 150000000) {
+    planetPass(2)
+  } else if (startDistance == 228000000) {
+    planetPass(3)
+  } else if (startDistance == 778000000) {
+    planetPass(4)
+  } else if (startDistance == 1430000000) {
+    planetPass(5)
+  } else if (startDistance == 2870000000) {
+    planetPass(6)
+  } else if (startDistance == 4500000000) {
+    planetPass(7)
+  } else if (startDistance == 5900000000) {
+    planetPass(8)
+  }
 }
